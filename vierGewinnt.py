be2e9747183b78b69d86ecf731ead1626c6dc6cb
Francois Kieninger <francois.kieninger@web.de>
2022-11-07 11:02:03 +0100

    Added basic window functionality (empty)

--- vierGewinnt.py
+++ vierGewinnt.py
@@ -21,3 +21,22 @@ UIPANEL_HEIGHT = SCREEN_HEIGHT - GRID_HEIGHT - PAD_VERTICAL
 UIMENU_WIDTH = SCREEN_WIDTH/3
 UILOG_WIDTH = SCREEN_WIDTH - UIMENU_WIDTH - PAD_HORIZONTAL
 
+FPS = 60
+pygame.init()
+clock = pygame.time.Clock()
+pygame.display.set_caption("4 Gewinnt")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+ui_manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+is_running = True
+while is_running:
+	time_delta = clock.tick(FPS) / 1000.0
+	clock.tick(FPS) 
+	for event in pygame.event.get(): 
+		if event.type == pygame.QUIT: 
+			is_running = False
+	
+	ui_manager.update(time_delta)
+	screen.fill(pygame.Color('#FFFFFF'))
+
+	pygame.display.flip()
