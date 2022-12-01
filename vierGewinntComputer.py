7a3f1e86c1f1ca6a28025719ed2e6497ad7e1729
Francois Kieninger <francois.kieninger@web.de>
2022-12-01 22:17:05 +0100

    vierGewinntStupidComputer should work now

--- vierGewinntComputer.py
+++ vierGewinntComputer.py
@@ -1,15 +1,20 @@
 from vierGewinntModel import *
+import copy
 
 # class for a node in the Tree, it's a linked list
 class vierGewinntNode:
-	def __init__(self, grid, parent):
+	def __init__(self, grid, playedCol, parent):
 		self._grid = grid
 		self._children = []
 		self._score = 0
 		self._parent = parent
+		self._col = playedCol
 
 	def getGrid(self):
 		return self._grid
+
+	def getCol(self):
+		return self._col
 	
 	def getChildren(self):
 		return self._children
@@ -26,14 +31,47 @@ class vierGewinntNode:
 	def setScore(self, score):
 		self._score = score
 
-# The class for the stupid AI
+# realy basic and retarded AI
+# It mistreats the vierGewinntNode class :(
 class vierGewinntStupidComputer:
-	def __init__(self):
-		self._model = 0
+	def __init__(self, playerSign):
+		self._model = vierGewinntModel()
+		self._sign = playerSign
 
-	def makeAMove(self, model):
-		self._model = model
+	def makeAMove(self, grid):
+		# generate the possibilites for the next move
+		baseGrid = copy.deepcopy(grid)
+		self._model.setGrid(baseGrid)
+		moves = []
+		for x in range(7):
+			self._model.play(x, self._sign)
+			node = vierGewinntNode(self._model.getGrid(), x, None)
+			if True:
+				winner = self._model.checkWinner()
+				if winner == 1:
+					node.setScore(500)
+				elif winner == 2:
+					node.setScore(-500)
+				else:
+					node.setScore(self.evaluateGameState(node.getGrid()))
+				moves.append(node)
+			self._model.setGrid(copy.deepcopy(grid))
 		
+		# search the best move
+		bestScore = 0
+		bestCol = 0
+		prefix = 1
+		if self._sign == 2:
+			prefix = -1
+		for node in moves:
+			if node.getScore()*prefix > bestScore:
+				bestScore = node.getScore()*prefix
+				bestCol = node.getCol()
+		
+		print(str(bestScore)+" at col "+str(bestCol))
+		return bestCol
+		
+
 
 	def evaluateGameState(self, grid):
 		# copied from https://softwareengineering.stackexchange.com/questions/263514/why-does-this-evaluation-function-work-in-a-connect-four-game-in-java
