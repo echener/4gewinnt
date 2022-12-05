edeb679068ec5c073730d65a4d63aa142d3b01f6
Francois Kieninger <francois.kieninger@web.de>
2022-12-05 09:36:59 +0100

    stupid AI should detect ennemy fours

--- vierGewinntComputer.py
+++ vierGewinntComputer.py
@@ -56,6 +56,22 @@ class vierGewinntStupidComputer:
 					node.setScore(self.evaluateGameState(node.getGrid()))
 				moves.append(node)
 			self._model.setGrid(copy.deepcopy(grid))
+
+		# try to find possible connect fours for the ennemy
+		self._model.setGrid(copy.deepcopy(grid))
+		sign = 1
+		if self._sign == 2:
+			sign = self._sign
+		for x in range(7):
+			self._model.play(x, sign)
+			node = vierGewinntNode(self._model.getGrid(), x, None)
+			winner = self._model.checkWinner()
+			if winner == sign:
+				if winner == 1:
+					node.setScore(475)
+				else:
+					node.setScore(-475)
+				moves.append(node)
 		
 		# search the best move
 		bestScore = 0
